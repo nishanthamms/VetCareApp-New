c6858fa0f44aa0ca3dedd5999c5624e367b66575
Nishantha <nishanthamms@gmail.com>
2021-02-27 21:03:30 +0530

    notification part added

--- src/app/Pages/add-vaccination/add-vaccination.page.ts
+++ src/app/Pages/add-vaccination/add-vaccination.page.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router } from '@angular/router';
 import { Vaccination } from 'src/app/models/Vaccine';
 import { VaccinationService } from '../../services/vaccination.service';
-
+import {Plugins, LocalNotificationEnabledResult,LocalNotificationActionPerformed,LocalNotification,Device} from '@capacitor/core'
+const {LocalNotifications} = Plugins;
+import {AlertController} from '@ionic/angular';
 @Component({
   selector: 'app-add-vaccination',
   templateUrl: './add-vaccination.page.html',
@@ -24,7 +26,7 @@ export class AddVaccinationPage implements OnInit {
   }
 
   constructor(private activatedRoute: ActivatedRoute,
-    private router: Router,
+    private router: Router,private alertCtrl: AlertController,
     private vaccinationService: VaccinationService) {
       this.ctid =  sessionStorage.getItem('cattleTagId');
      }
@@ -46,10 +48,34 @@ export class AddVaccinationPage implements OnInit {
       this.vaccine.nextVaccineDate= '';
       this.vaccine.reasonOfNextVaccine= '';
       this.vaccine.remarks= '';
+
+      this.sheduleNotification(this.vaccine.nextVaccineDate,this.vaccine.cattleid);
     }, err => {
     });
   }
+  
+  async sheduleNotification(vacinedate,cattleid){
 
+    await LocalNotifications.schedule({ 
+      notifications: [
+        {
+          title: 'Vaccination Reminder',
+          body: 'To Day is vaccination day of ' +cattleid+' cattle',
+          id:2,
+          extra:{
+            data: 'Pass data to your handler'
+          },
+          iconColor: '#00e64d',
+         // actionTypeId: 'CHAT_MSG',
+          /*attachments:[
+            {id:'face',url:'res://public/assets/vaccination_icon.png'}
+          ],*/
+          schedule: {at:new Date(vacinedate)},
+        }
+      ]
+    });
+   }
+  
 
   goback(){
    
